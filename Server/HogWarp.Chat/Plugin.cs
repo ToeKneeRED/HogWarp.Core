2c695392aab4117bec217cc6d107b06f1b7fc74b
maximegmd <672982+maximegmd@users.noreply.github.com>
2025-02-18 23:28:07 +0100

    Base structure

---
+++ Server/HogWarp.Chat/Plugin.cs
@@ -0,0 +1,150 @@
+﻿using HogWarp.Replicated;
+using HogWarpSdk.Game;
+using HogWarpSdk.Systems;
+using System.Numerics;
+
+namespace HogWarpChat
+{
+    public class Plugin : HogWarpSdk.IPlugin
+    {
+        private Logger log = new Logger("HogWarpChat");
+        public event Action<Player, string> OnChatMessage;
+        private float sayDist = 400;
+        private float shoutDist = 1500;
+        private float whisperDist = 100;
+        private BP_HogWarpChat? chatActor;
+        public bool chatMsgOverride = false;
+        private Dictionary<string, Action<Player, string>> commands = new Dictionary<string, Action<Player, string>>();
+
+        public Plugin()
+        {
+            OnChatMessage += Chat_OnChatMessage;
+
+
+        }
+
+        public string Author => "HogWarp Team";
+
+        public string Name => "HogWarpChat";
+
+        public Version Version => new(1, 0);
+
+        public void PostLoad()
+        {
+            chatActor = HogWarpSdk.Server.World.Spawn<BP_HogWarpChat>()!;
+            chatActor.Plugin = this;
+
+            commands.Add("/me", SlashMe);
+            commands.Add("/house", SlashHouse);
+            commands.Add("/say", SlashDistMsg);
+            commands.Add("/shout", SlashDistMsg);
+            commands.Add("/whisper", SlashDistMsg);
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        enum House
+        {
+            Gryffindor,
+            Hufflepuff,
+            Ravenclaw,
+            Slytherin,
+            Unaffiliated
+        }
+
+        public void AddCommand(string command, Action<Player, string> action)
+        {
+            if (!commands.TryAdd(command, action))
+            {
+                log.Warn($"{command} command already exists!");
+            }
+        }
+
+        public void ReceiveMessage(Player player, string msg)
+        {
+            if (OnChatMessage != null)
+                OnChatMessage.Invoke(player, msg);
+        }
+
+        public void SendMessage(Player player, string msg)
+        {
+            if (chatActor != null)
+                chatActor.RecieveMsg(player, msg);
+        }
+
+        private void SlashMe(Player player, string msg)
+        {
+            foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+            {
+                SendMessage(p, "<Server>" + player.Username + msg.Substring(3) + "</>");
+            }
+        }
+
+        private void SlashHouse(Player player, string msg)
+        {
+            foreach (var p in HogWarpSdk.Server.PlayerSystem.Players.Where(p => p.House == player.House))
+            {
+                SendMessage(p, "<img id=\"" + (House)player.House + "\"/><" + (House)player.House + ">" + player.Username + ": " + msg.Substring(7) + "</>");
+            }
+        }
+
+        private void SlashDistMsg(Player player, string msg)
+        {
+            float msgDist = 0;
+            int msgSub = 0;
+            string msgType = "says";
+
+            if (msg.StartsWith("/say")) { msgDist = sayDist; msgSub = 5; msgType = "says"; }
+            else if (msg.StartsWith("/shout")) { msgDist = shoutDist; msgSub = 7; msgType = "shouts"; }
+            else { msgDist = whisperDist; msgSub = 9; msgType = "whispers"; }
+
+            foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+            {
+                Vector3 plyPos = new Vector3(player.Position.X, player.Position.Y, player.Position.Z);
+                Vector3 pPos = new Vector3(p.Position.X, p.Position.Y, p.Position.Z);
+                var dist = plyPos - pPos;
+
+                if (dist.Length() <= msgDist)
+                {
+                    SendMessage(p, player.Username + " " + msgType + ": " + msg.Substring(msgSub));
+                }
+            }
+        }
+
+        private void BuildMessage(Player player, string msg)
+        {
+            log.Info(player.Username + ": " + msg);
+
+            if (commands.ContainsKey(msg.Split(" ")[0]))
+            {
+                commands[msg.Split(" ")[0]].DynamicInvoke(player, msg);
+            }
+            else
+            {
+                foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+                {
+                    SendMessage(p, "<img id=\"" + (House)player.House + "\"/><" + (House)player.House + ">" + player.Username + ": </>" + msg);
+                }
+            }
+        }
+        private void Chat_OnChatMessage(Player player, string msg)
+        {
+            if (!chatMsgOverride)
+                BuildMessage(player, msg);
+        }
+    }
+}
+
+namespace HogWarp.Replicated
+{
+    public partial class BP_HogWarpChat
+    {
+        internal HogWarpChat.Plugin? Plugin { get; set; }
+        public partial void SendMsg(Player player, string Message)
+        {
+            Plugin!.ReceiveMessage(player, Message);
+        }
+    }
+}
---
+++ Server/HogWarp.Weather/Plugin.cs
@@ -0,0 +1,153 @@
+﻿using HogWarp.Replicated;
+using HogWarpSdk.Game;
+using HogWarpSdk.Systems;
+
+namespace HogWarpWeather
+{
+    public class Plugin : HogWarpSdk.IPlugin
+    {
+        public string Author => "HogWarp Team";
+        public string Name => "HogWarpWeather";
+        public Version Version => new(1, 0);
+        enum Seasons
+        {
+            Invalid = 0,
+            Fall = 1,
+            Winter = 2,
+            Spring = 3,
+            Summer = 4,
+        }
+
+        private Seasons currentSeason = Seasons.Fall;
+        private string currentWeather = "StormyLarge_01";
+        private DateTime currentTime = new DateTime(1980, 1, 1, 6, 0, 0);
+        private Logger log = new HogWarpSdk.Systems.Logger("HogWarpWeather");
+        private BP_HogWarpWeather? weatherActor;
+
+        public Plugin()
+        {
+        }
+
+        private void PlayerSystem_PlayerJoinEvent(Player Id)
+        {
+            if (weatherActor != null)
+            {
+                weatherActor.UpdateTime(Id, currentTime.Hour, currentTime.Minute, currentTime.Second);
+                weatherActor.UpdateSeason(Id, (int)currentSeason);
+                weatherActor.UpdateWeather(Id, currentWeather);
+            }
+        }
+
+        public void PostLoad() 
+        {
+            weatherActor = HogWarpSdk.Server.World.Spawn<BP_HogWarpWeather>()!;
+            weatherActor.Plugin = this;
+            
+            HogWarpSdk.Server.PlayerSystem.PlayerJoinEvent += PlayerSystem_PlayerJoinEvent;
+            HogWarpSdk.Server.Timer.Add(Timer_Elapsed, 60.0f);
+
+            log.Info($"The time is {currentTime.TimeOfDay.ToString()}, the weather is: {currentWeather}, and the season: {currentSeason.ToString()}");
+        }
+
+        private void Timer_Elapsed(float delta)
+        {
+            currentTime.AddMinutes(1);
+            SendTimeUpdate();
+            if (weatherActor != null)
+            {
+                if(HogWarpSdk.Server.PlayerSystem.Players.First() != null)
+                {
+                    weatherActor.RequestWeather(HogWarpSdk.Server.PlayerSystem.Players.First());
+                }
+            }
+        }
+
+        public void Shutdown() 
+        {
+
+        }
+
+        public void SetWeather(string weather)
+        {
+            if (weather != currentWeather)
+            {
+                currentWeather = weather;
+
+                if (weatherActor != null)
+                {
+                    foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+                    {
+                        weatherActor.UpdateWeather(p, currentWeather);
+                    }
+                }
+            }
+        }
+
+        public void SetSeason(int season)
+        {
+            if (season != (int)currentSeason)
+            {
+                currentSeason = (eSeasons)season;
+
+                if (weatherActor != null)
+                {
+                    foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+                    {
+                        weatherActor.UpdateSeason(p, (int)currentSeason);
+                    }
+                }
+            }
+        }
+
+        public void SetTime(int hour, int min, int sec)
+        {
+            var newTime = new DateTime(1980, 1, 1, hour, min, sec);
+
+            if (newTime != currentTime)
+            {
+                currentTime = newTime;
+
+                if (weatherActor != null)
+                {
+                    foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+                    {
+                        weatherActor.UpdateTime(p, currentTime.Hour, currentTime.Minute, currentTime.Second);
+                    }
+                }
+            }
+        }
+
+        private void SendTimeUpdate()
+        {
+            if (weatherActor != null)
+            {
+                foreach (var p in HogWarpSdk.Server.PlayerSystem.Players)
+                {
+                    weatherActor.UpdateTime(p, currentTime.Hour, currentTime.Minute, currentTime.Second);
+                }
+            }
+        }
+    }
+}
+
+namespace HogWarp.Replicated
+{
+    public partial class BP_HogWarpWeather
+    {
+        internal HogWarpWeather.Plugin? Plugin { get; set; }
+        public partial void SendWeather(HogWarpSdk.Game.Player player, string Weather)
+        {
+            Plugin!.SetWeather(Weather);
+        }
+
+        public partial void SendSeason(HogWarpSdk.Game.Player player, int Season)
+        {
+            Plugin!.SetSeason(Season);
+        }
+
+        public partial void SendTime(HogWarpSdk.Game.Player player, int Hour, int Minute, int Second)
+        {
+            Plugin!.SetTime(Hour, Minute, Second);
+        }
+    }
+}
